73149f141ba73b927057e6f6ed03dbc76102a84a
Nelson drew <nelsondrew07@gmail.com>
2023-10-20 21:34:24 +0530

    cors changes

--- posts/index.js
+++ posts/index.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser")
-const { randomBytes}  = require("crypto")
+const { randomBytes } = require("crypto")
+const cors = require("cors")
 
 const app = express();
 app.use(bodyParser.json())
+app.use(cors())
 
 const posts = {};
 
 
-app.get('/posts' , (req ,res) => {
- res.send(posts)
+app.get('/posts', (req, res) => {
+  res.send(posts)
 })
 
-app.post('/posts' , (req ,res) => {
- const {title} = req.body;
- const id  = randomBytes(4).toString("hex")
- posts[id] = {
-   id : id,
-   title : title
- }
+app.post('/posts', (req, res) => {
+  const { title } = req.body;
+  const id = randomBytes(4).toString("hex")
+  posts[id] = {
+    id: id,
+    title: title
+  }
 
- res.status(201).send(posts)
+  res.status(201).send(posts)
 })
 
-app.listen(4000 , () => {
-    console.log("Listening on 4000")
+app.listen(4000, () => {
+  console.log("Listening on 4000")
 })
